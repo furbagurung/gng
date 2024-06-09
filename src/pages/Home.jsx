1383135dae83840f1a05e480ad484f83fcd32881
furbagurung <foorvaa.grg@gmail.com>
2024-06-10 00:40:20 +0545

    poppins bold add gareko title ma

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import CategoryCard from "@/components/ui/CategoryCard";
 import { FAQ } from "./FAQ";
-import CaraouselSlider from "@/components/ui/CaraouselSlider";
+// import CaraouselSlider from "@/components/ui/CaraouselSlider";
 
 import "../App.css";
 import hero from "../assets/Hero.jpg";
@@ -17,7 +17,6 @@ import LipOil from "../assets/LipOil.png";
 import Mist from "../assets/Mist.png";
 import Stick from "../assets/Stick.png";
 
-import { motion } from "framer-motion";
 import { Button } from "../components/ui/button";
 import { ChevronRightIcon } from "@heroicons/react/24/outline";
 import ProductCard from "@/components/ui/ProductCard";
@@ -31,17 +30,17 @@ function Home() {
         </div>
 
         <div className="min-w-[300px] max-w-[80%] grid  justify-center items-center gap-1 absolute bottom-8">
-          <h1 className=" font-bold sm:text-4xl text-3xl leading-tight font-sf text-center tracking-tight">
+          <h1 className=" font-bold sm:text-4xl text-3xl leading-tight poppins-bold text-center tracking-tight">
             Discover the secrets of Beauty products
           </h1>
-          <p className=" leading-tight text-center text-[16px] font-medium poppins-regular">
+          <p className="  text-center text-[16px] font-normal poppins-regular">
             Shop now the best BB cream cushion foundation
           </p>
           <div className="flex justify-center mt-4">
             <Button variant="default" width="200px">
               <a
                 href="#"
-                className=" underline text-[13px] font-medium py-2 px-4 rounded poppins-regular"
+                className="  text-[13px] font-medium py-2 px-4 rounded poppins-regular"
               >
                 {" "}
                 Contact Us
@@ -52,7 +51,7 @@ function Home() {
       </section>
       <section className="min-w-[320px] lg:h-[375px] h-[650px] grid  w-full justify-center relative p-4">
         <div className=" lg:w-full grid justify-center gap-2 py-16 mb-6 ">
-          <h1 className="font-bold text-4xl  text-center lg:text-5xl leading-tight font-sf tracking-tight">
+          <h1 className="text-4xl  text-center lg:text-5xl leading-tight  poppins-bold  tracking-tight">
             Shop our Collection
           </h1>
           <p className=" leading-tight flex  text-center text-[16px] font-medium poppins-regular">
@@ -70,7 +69,7 @@ function Home() {
       {/* why Gossip */}
       <section className="lg:h-[500px] min-w-[320px] h-[500px] bg-black  flex flex-col relative  w-full items-center px-6">
         <div className="  w-full flex flex-col text-white justify-center items-center gap-1 pt-40 pb-10 lg:pt-[150px]">
-          <h1 className="w-full text-center font-bold text-4xl lg:text-5xlleading-tight font-sf tracking-tight">
+          <h1 className="w-full text-center font-bold text-4xl lg:text-5xlleading-tight poppins-bold tracking-tight">
             What makes us Gossips?
           </h1>
           <p className=" lg:w-[700px] min-w-[320px] p-4 text-center leading-tight flex justify-center text-[16px] font-normal poppins-regular">
@@ -99,7 +98,7 @@ function Home() {
       {/* sustainable */}
       <section className="lg:h-[600px] h-[450px] bg-white flex flex-col w-full relative md:pt-[50px] p-4 ">
         <div className="  w-full grid justify-center grid-rows-auto-rows-fr gap-4 pt-32">
-          <h1 className="font-bold text-4xl lg:text-5xl leading-tight text-center font-sf tracking-tight">
+          <h1 className="font-bold text-4xl lg:text-5xl leading-tight text-center  poppins-bold  tracking-tight">
             Sustainable Beauty
           </h1>
           <p className=" leading-tight text-center text-[16px] font-medium poppins-regular">
@@ -113,7 +112,7 @@ function Home() {
       {/* Best Selling */}
       <section className=" h-fit flex flex-col relative  w-full items-center p-4">
         <div className="  w-full flex flex-col  justify-center items-center gap-1 pt-[100px]">
-          <h1 className="w-full text-center font-bold text-4xl lg:text-5xl leading-tight font-sf tracking-tight">
+          <h1 className="w-full text-center font-bold text-4xl lg:text-5xl leading-tight poppins-bold  tracking-tight">
             Best Sellings
           </h1>
           <p className=" leading-tight text-center text-[16px] font-medium poppins-regular">
@@ -151,7 +150,7 @@ function Home() {
       {/* Certification */}
       <section className="h-auto flex flex-col bg-white w-full relative pt-[50px]">
         <div className="  w-full grid justify-center grid-rows-auto-rows-fr gap-4 lg:pt-[10px] ">
-          <h1 className="font-bold text-5xl text-center leading-tight font-sf tracking-tight">
+          <h1 className="font-bold text-5xl text-center leading-tight  poppins-bold tracking-tight">
             Certification
           </h1>
           <p className=" leading-tight flex justify-center text-[16px] font-medium poppins-regular">
@@ -166,8 +165,8 @@ function Home() {
       {/* FAQ */}
       <section className="text-white bg-black flex flex-col w-full relative pb-11 p-4 ">
         <div className="  w-full grid gap-4 py-16 ">
-          <h1 className="font-bold md:text-4xl text-3xl justify-center lg:text-center text-left leading-tight font-sf tracking-tight">
-            <p className=" leading-tight justify-center px-1 lg:flex grid sm:text-center text-left  font-medium poppins-regular">
+          <h1 className="font-bold md:text-4xl text-3xl justify-center lg:text-center text-left leading-tight  poppins-bold racking-tight">
+            <p className=" leading-tight justify-center px-1 lg:flex grid sm:text-center text-left poppins-bold ">
               Frequently Asked Questions
             </p>
           </h1>
