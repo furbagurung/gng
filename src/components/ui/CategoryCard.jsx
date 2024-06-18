26a2592a6e4b7f030148e26e9b4279ca5c38d235
furbagurung <foorvaa.grg@gmail.com>
2024-06-19 00:32:21 +0545

    category card nested <a> fixed

--- src/components/ui/CategoryCard.jsx
+++ src/components/ui/CategoryCard.jsx
@@ -9,9 +9,9 @@ const CategoryCard = ({ title, src, pathname }) => {
       <motion.div className="grid justify-center bg-black md:max-w-[200px] min-w-[100px] rounded-2xl lg:rounded-2xl text-white  m-1 pt-3 z-10 gap-2 cursor-pointer">
         <NavLink to={pathname}>
           <div className="flex justify-between px-4">
-            <a href="" className="font-bold poppins-bold">
+            <p href="" className="font-bold poppins-bold">
               {title}
-            </a>
+            </p>
             <ArrowRightCircleIcon className="h-6 w-6 text-white transform -rotate-45" />
           </div>
           <div className="min-w-[100px] m-2  rounded-xl lg:rounded-xl overflow-clip">
@@ -25,6 +25,6 @@ const CategoryCard = ({ title, src, pathname }) => {
 CategoryCard.propTypes = {
   title: PropTypes.string.isRequired,
   src: PropTypes.string.isRequired,
-  pathname: PropTypes.string.isRequired,
+  pathname: PropTypes.string,
 };
 export default CategoryCard;
