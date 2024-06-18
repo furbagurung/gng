245a227fc824bb8693ea390a2e7fd9d7e5136b4d
furbagurung <foorvaa.grg@gmail.com>
2024-06-19 02:03:02 +0545

    eyebrow

--- src/pages/Eyebrow.jsx
+++ src/pages/Eyebrow.jsx
@@ -1,31 +1,24 @@
 import CatalogLayout from "../layouts/CatalogLayout";
-
-import i1 from "../assets/eyebrow/eyebrow1.webp";
-import i2 from "../assets/eyebrow/eyebrow2.webp";
-import i3 from "../assets/eyebrow/eyebrow3.webp";
-import i4 from "../assets/eyebrow/eyebrow4.webp";
-import i5 from "../assets/eyebrow/eyebrow5.webp";
+// import img from "../../assets/blusher";
+import i1 from "../assets/cushion/cushion1.webp";
+import i2 from "../assets/cushion/cushion2.webp";
+import i3 from "../assets/cushion/cushion3.webp";
+import i4 from "../assets/cushion/cushion4.webp";
+import i5 from "../assets/cushion/cushion5.webp";
 
 function Eyebrow() {
   const description = () => {
     return (
       <>
         <p>
-          Lipstick, a beloved cosmetic product, adds color and moisture to the
-          lips, enhancing their natural beauty. Available in a wide range of
-          shades and finishes, lipsticks offer endless possibilities for
-          creating different looks and styles. They come in creamy, matte,
-          satin, and glossy formulas, providing options for various preferences
-          and occasions. Lipsticks are known for their pigmentation, smooth
-          application, and long-lasting wear. With a single swipe, they can
-          transform the lips, adding a touch of glamour and completing any
-          makeup look. From bold and vibrant to subtle and natural, lipsticks
-          are a staple in every makeup collection, allowing individuals to
-          express their personal style and boost their confidence.
+          A weightless, long-lasting liquid blush that blends and builds
+          beautifully for a soft, healthy flush. Available in both matte and
+          dewy finishes.
         </p>
       </>
     );
   };
+
   const list = [
     "Highly Pigmented",
     "Blends effortlessly",
@@ -36,9 +29,9 @@ function Eyebrow() {
   return (
     <>
       <CatalogLayout
-        title="Eyebrow"
+        title="Eye Brow"
         description={description()}
-        price={13}
+        price={15}
         features={list}
         img={[i1, i2, i3, i4, i5]}
       />
