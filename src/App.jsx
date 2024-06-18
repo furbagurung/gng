a18bb7047826a37a8c1dcf5f9fc6bfd8c9b63aee
furbagurung <foorvaa.grg@gmail.com>
2024-06-19 01:14:03 +0545

    foundation

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import Privacy from "./pages/Privacy.jsx";
 
 import Tnc from "@/pages/Tnc.jsx";
 import Cushionfoundation from "@/pages/catalog/cushionfoundation.jsx";
-import Foundation from "@/pages/catalog/Foundation.jsx";
+import Foundation from "./pages/catalog/Foundation.jsx";
 // import Primer from "@/pages/catalog/Primer.jsx";
 import Facepowder from "@/pages/catalog/Facepowder.jsx";
 import Blusher from "@/pages/catalog/Blusher.jsx";
